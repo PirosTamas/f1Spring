5980bb7a890375f5a82a12b7be29cccef930a4da
TamasPiros <tomi.piros@gmail.com>
2023-01-15 17:15:24 +0100

    update security

--- src/main/java/com/tomisoft/f1/security/DBUserDetailsService.java
+++ src/main/java/com/tomisoft/f1/security/DBUserDetailsService.java
@@ -2,10 +2,13 @@ package com.tomisoft.f1.security;
 
 import com.tomisoft.f1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Hibernate;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +17,10 @@ public class DBUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new UserPrincipal(this.userRepository.findByUsername(username));
+        var user = this.userRepository.findByUsername(username);
+        Hibernate.initialize(user.getRoles());
+        return new UserPrincipal(user);
     }
 }
